cf1766681594de884ac3cc27c9e6cba001cf1137
riza <rizagungor@gmail.com>
2021-08-23 16:16:03 +0300

    '1'

--- anasayfa.php
+++ anasayfa.php
@@ -1,6 +1,6 @@
 			  <?php 
 
-$seviye=array(21);
+$seviye=array(1);
 
 		if (!isset($_SESSION)) {session_start();}
 
@@ -230,4 +230,4 @@ include_once("ansayfaalt.php");
 
 
 
-?>		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  
+?>		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  
