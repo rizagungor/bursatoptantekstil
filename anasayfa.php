c8d73ed051fb311aea7af7ad08dd7137faac8479
riza <rizagungor@gmail.com>
2021-08-23 15:30:52 +0300

    Son bu dur

--- anasayfa.php
+++ anasayfa.php
@@ -1,6 +1,6 @@
 			  <?php 
 
-$seviye=array(1,2,3,4,5,6,7);
+$seviye=array(1,2,3,4,5,6,7,8);
 
 		if (!isset($_SESSION)) {session_start();}
 
@@ -230,4 +230,4 @@ include_once("ansayfaalt.php");
 
 
 
-?>		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  
+?>		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  		  
